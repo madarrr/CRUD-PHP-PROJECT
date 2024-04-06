0d96fda275f646ff9bdac8b0f597571f8fa9d3da
madarrr <delaureaka@gmail.com>
2024-04-06 10:32:50 +0000

    set of the config file for db connection

---
+++ config/app.php
@@ -0,0 +1,10 @@
+<?php
+// define the hostname or ip adress of the mysql server local host means the server is on the same machine where the php script is execute
+define('DB_HOST','localhost');
+// define db user in localhost environnement
+define('DB_USER','root');
+// define password for user in local host
+define('DB_PASSWORD','');
+// define db name 
+define('DB_DATABASE','oopdb');
+?>
